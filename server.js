18508e048743eb7e763f09581c843915d4fff839
Ark <arbaazkhanark23@gmail.com>
2023-02-01 20:53:48 +0530

    Multiple Route Pages in Express

--- server.js
+++ server.js
@@ -8,8 +8,22 @@ const app=express();
 
 /// It will Solve the (cannot GET) Error,,,  Because It will get and send info.
 app.get("/",function(reqest,respose){
-    respose.send("<h1>Wellcome to my Server</h1>")
+    respose.send("<h1>Wellcome to my Server</h1>");
 });
+
+app.get("/contact",function(req,res){
+    res.send("<p>Contact Me At: arbaazkhanark23@gmail.com</p>");
+});
+
+app.get("/about",function(req,res){
+    res.send("<h3>My name is Arbaaz Khan && I Love to do Code..</h3>");
+});
+
+app.get("/hobbies",function(req,res){
+    // res.send("<p>Coding</p>");
+    res.send("<p>football</p>");      // Only One send is valid
+});
+
 
 const Port=3000;
 app.listen(Port,function(){
