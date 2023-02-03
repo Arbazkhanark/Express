66df7364895dfaa104ea0a1dd367605d18daf4c5
Ark <arbaazkhanark23@gmail.com>
2023-02-04 03:32:25 +0530

    -----Weather API Showing On WebPage :) -----

--- Weather_API_Project3/app.js
+++ Weather_API_Project3/app.js
@@ -1,31 +1,35 @@
 const express=require("express");
-
 const https=require("https");
-
 const app=express();
 
 
 app.get("/",function(req,res){
 
-    var url="https://api.openweathermap.org/data/2.5/weather?q=India&appid=a791b9299ff1bb320e9512883120b89b";
+    const url="https://api.openweathermap.org/data/2.5/weather?q=delhi&country=india&appid=a791b9299ff1bb320e9512883120b89b&units=metric";
+    
     https.get(url,function(response){
-        console.log(response);
+        console.log(response.statusCode);
 
 
         response.on("data",function(data){
             // console.log(data);   HexaCode ans
-            
+            const weatherData=JSON.parse(data)
+            const temp=weatherData.main.temp
+            const weatherDescription=weatherData.weather[0].description
             const weatherReport= JSON.parse(data); //JSON.parse:-convert the data into JSON format 3D(3D JSON).:-  Multi-Lines
-            // console.log(weatherReport);
 
-            console.log(JSON.stringify(weatherReport)); //JSON.stringify:- convert the Object format to String format 2D(raw JSON).:- Single-Line 
-        })
+            // important for use HTML tags inside res.write
+            res.setHeader("Content-Type", "text/html"); /// IMPORTANT-> USE HTML TAGS
+            res.write(`<p>The weather is currently ${weatherDescription}.</p>`); //res.write:- will help us to send multiple "res.write"=(inverse of "res.send"). 
+            res.write(`<h1> The temperature in <i> Delhi,India </i>is ${temp} <i> degree </i> Celcius.</h1>`);
+            res.send()  // res.send :- will help us to send the request :: It should be one in app (inserve of res.write)
+        });
     });
 
     
 
 
-    res.send("Wellcome to My server..");
+    // res.send("Wellcome to My server..");
 });
 
 
